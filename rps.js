246daf34de818b1dd5a469ac8d0ef1434f2f5aeb
BW <17097491+bw-01@users.noreply.github.com>
2024-07-07 13:30:11 +1200

    Refactored code to add buttons for player to click.

--- rps.js
+++ rps.js
@@ -1,27 +1,24 @@
 //A quick game of rock, paper, scissors, implemented via the console.
 //Author: BW-01
 
-const choices = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 
 //Randomly generate computer choice
 function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 }
 
-//Prompt user for their choice
-function getHumanChoice() {
-  let choice = prompt("Type a word to choose 'rock', 'paper', or 'scissors'").toLowerCase();
-  while (!choices.includes(choice)) {
-    console.log("Invalid choice!");
-    choice = prompt(
-      "Invalid choice! Enter the word 'rock', 'paper', or 'scissors' without the surrounding quotes"
-    ).toLowerCase();
-  }
-  return choice;
-}
+let buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (event) => {
+  let humanChoice = event.target.textContent;
+  let computerChoice = getComputerChoice();
+
+  playRound(humanChoice, computerChoice);
+});
 
 //Play a single round of the game
 function playRound(humanChoice, computerChoice) {
@@ -42,17 +39,12 @@ function playRound(humanChoice, computerChoice) {
 
 //Play multiple rounds of the game
 function playGame() {
-  console.log("Welcome to Rock, Paper, Scissors. The game will play for 5 rounds.");
   for (round = 1; round <= 5; round++) {
     console.log("Round " + round);
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
+    // let humanChoice = getHumanChoice();
+    // let computerChoice = getComputerChoice();
 
-    // make first letter uppercase
-    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1);
-    computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1);
-
-    playRound(humanChoice, computerChoice);
+    // playRound(humanChoice, computerChoice);
   }
 
   if (humanScore > computerScore) {
@@ -62,4 +54,4 @@ function playGame() {
   }
 }
 
-playGame();
+// playGame();
